5f97e088897bb487fe2aa2dfb0a9e3fdcbeb0a03
Pascal DAUTRESME <pascaldaut@hotmail.com>
2026-04-20 14:23:28 +0200

    Add custom domain configuration for Azure Static Web App

--- modules/app_service_static_web_app/static_web_app.tf
+++ modules/app_service_static_web_app/static_web_app.tf
@@ -12,6 +12,18 @@ resource "azurerm_static_web_app" "static_web_app" {
   repository_url    = var.repository_url
   repository_branch = var.repository_branch
   repository_token  = var.repository_token
-
 }
-  
+
+# resource "azurerm_dns_cname_record" "online_cname" {
+#   name                = var.custom_domain_name
+#   zone_name           = var.dns_zone_name
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   ttl                 = 300
+#   record              = azurerm_static_web_app.static_web_app.default_host_name
+# }
+
+# resource "azurerm_static_web_app_custom_domain" "custom_domain_name" {
+#   static_web_app_id = azurerm_static_web_app.static_web_app.id
+#   domain_name       = "${var.custom_domain_name}.${var.dns_zone_name}"
+#   validation_type   = "cname-delegation"
+# }
--- modules/app_service_static_web_app/variables.tf
+++ modules/app_service_static_web_app/variables.tf
@@ -32,3 +32,16 @@ variable "repository_token" {
   type        = string
   sensitive   = true
 }
+
+variable "custom_domain_name" {
+  description = "The custom domain name to be associated with the static web app."
+  type        = string
+  default     = "www"
+
+}
+variable "dns_zone_name" {
+  description = "The name of the DNS zone where the CNAME record will be created."
+  type        = string
+  default     = "dautresmep.online"
+
+}
